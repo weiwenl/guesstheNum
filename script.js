772c3bfdbea1b6f9779a26269b92da224bfd101b
weiwenl <weiwenl.justin@gmail.com>
2018-09-14 14:45:47 +0800

    First commit for all files w/ pseudo code

---
+++ script.js
@@ -0,0 +1,22 @@
+//Pseudo Code
+
+//Get 4 numbers as input (0-9, no repeats)
+//- Alert error if the numbers are repeated
+//- if no input detected get input = random numbers
+//- Save number
+//- hide the 4 numbers from view of player when <start game> button is clicked
+
+//Create 4 input element to get the guess from the user
+//Check if the input is the same as the SET INPUT
+//- if the number exists in the array, set the background color of the input to RED
+//- if the number exists and it is in the correct position in the array,
+    //set the background color of the input to GREEN
+//- if the number does not exist at all, dont change the background color
+
+//After the check process is done, create another set of elements of the next guess
+//Call the fn above and repeat the process until the player has reach 7 guess.
+
+//Keep checking that the input = to the set input < 8 guess
+//- If the user did not solve the guess within 7 tries, alert user that he lost.
+
+// |_| |_| |_| |_|
